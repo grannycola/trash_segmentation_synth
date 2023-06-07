e6da995e6a243e784c792967ba577c2dc51a87c1
Yaroslav Naychuk <twisterjesus@yandex.ru>
2023-06-08 00:09:04 +0300

    Tested other backbones, refactored code

--- dataloaders.py
+++ dataloaders.py
@@ -14,6 +14,9 @@ from pycocotools.coco import COCO
 def to_tensor(x, **kwargs):
     return x.transpose(2, 0, 1).astype('float32')
 
+# def to_tensor_mask(x, **kwargs):
+#     x = x.transpose(2, 0, 1).astype('float32')
+#     return np.squeeze(x, axis=0)
 
 def get_preprocessing(preprocessing_fn):
     _transform = [
@@ -61,45 +64,39 @@ class TacoDatasetSegmentation(Dataset):
         ]
         return A.Compose(transform)
     
-    # Use supercategories instead classes
-    def getSupercategory(self, category_id):
-        f = open(self.coco_annotation_file_path)
-        cat_json_file = json.load(f)
-        
-        for category in cat_json_file['categories']:
-            if category_id == category['id']:
-                f = open('data/supercategories.json')
-                supcat_json_file = json.load(f)
-                return supcat_json_file[category['supercategory']]
-    
-    def __init__(self, json_ann_file, input_size=(512, 512), classes=None,):
+    def __init__(self, json_ann_file=None, input_size=(512, 512), classes=None,):
         super(TacoDatasetSegmentation, self).__init__()
+        #self.coco_annotation_file_path = json_ann_file
+        #self.coco_annotation = COCO(annotation_file=self.coco_annotation_file_path)
+        #self.img_ids = self.coco_annotation.getImgIds()
         
-        self.coco_annotation_file_path = json_ann_file
-        self.coco_annotation = COCO(annotation_file=self.coco_annotation_file_path)
-        
-        self.img_ids = self.coco_annotation.getImgIds()
+        self.masks_files_list = os.listdir('data/resized/masks_2_classes/')
         self.input_size = input_size
 
     def __getitem__(self, index):
-        # Get images
-        img_id = self.img_ids[index]
-        img_info = self.coco_annotation.loadImgs(img_id)[0]
+
+        #img_id = self.img_ids[index]
+        #img_info = self.coco_annotation.loadImgs(img_id)[0] 
+        #img_path = 'data/resized/' + img_info["file_name"]
+        #mask_filename = img_info["file_name"].replace('images/','').replace('.jpg', '.png')
         
-        img_path = 'data/resized/' + img_info["file_name"]
-        image = cv2.imread(img_path)
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
-        # Get Mask
-        mask_filename = img_info["file_name"].replace('images/','').replace('.jpg', '.png')
+        mask_filename = self.masks_files_list[index]
         path_to_mask = 'data/resized/masks_2_classes/' + mask_filename
+        
+        
+        img_path = 'data/resized/images/' + mask_filename
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+   
         mask = cv2.imread(path_to_mask, cv2.IMREAD_GRAYSCALE)
         mask = np.expand_dims(mask, axis=2)
         
         return image, mask
 
     def __len__(self):
-        return len(self.img_ids)
+        return len(self.masks_files_list)
     
     
 class TacoLoaders:
@@ -112,11 +109,10 @@ class TacoLoaders:
 
         return torch.utils.data.random_split(dataset, [train_size, test_size])
 
-    def __init__(self, preprocessing_fn, batch_size = 4, augmentation=None, resize_input=None,
+    def __init__(self, preprocessing_fn, batch_size = 4, augmentation=None,
                  train_size_perc=0.9):
 
-        self.train_dataset = TacoDatasetSegmentation('data/annotations.json')
-        #self.test_dataset = TacoDatasetSegmentation('data/annotations_0_test.json')
+        self.train_dataset = TacoDatasetSegmentation()
         
         # Сначала сплитим данные, потом аугментации
         self.train_dataset, self.valid_dataset = TacoLoaders.train_test_split(self.train_dataset,
@@ -134,12 +130,12 @@ class TacoLoaders:
                                                 preprocessing_fn),
                                             )
         self.valid_dataset = ApplyTransform(self.valid_dataset,
-                                            augmentation=resize_input,
+                                            augmentation=None,
                                             preprocessing=get_preprocessing(
                                                 preprocessing_fn),
                                             )
         self.test_dataset = ApplyTransform(self.test_dataset,
-                                           augmentation=resize_input,
+                                           augmentation=None,
                                            preprocessing=get_preprocessing(
                                                preprocessing_fn),
                                            )
@@ -155,4 +151,6 @@ class TacoLoaders:
         n = np.random.choice(len(self.train_dataset))
         image, mask = self.train_dataset[n]
         visualize(image=image.transpose((1, 2, 0)),
-                  mask=mask.transpose((1, 2, 0)) * 255)    
+                  mask=mask.transpose((1, 2, 0)) * 255 ### if you use DiceLoss
+                  #mask=mask * 255 
+                 )    
